75695e6882977528df09dc38175f666c9f189bec
Jack <harri109@lancaster.ac.uk>
2022-12-02 22:00:13 +0000

    added day 3 test skelton

--- src/__tests__/examples.test.ts
+++ src/__tests__/examples.test.ts
@@ -1,14 +1,22 @@
 import {solutions} from "../solutions"
 
 describe("examples", () => {
+
     it("day01", () => {
       const {part1, part2} = solutions(1, true)
       expect(part1()).toBe(24000);
       expect(part2()).toBe(45000);
     });
+
     it("day02", () => {
       const {part1, part2} = solutions(2, true)
       expect(part1()).toBe(15);
       expect(part2()).toBe(12);
+    });
+
+    it("day03", () => {
+      // const {part1, part2} = solutions(3, true)
+      // expect(part1()).toBe(0);
+      // expect(part2()).toBe(0);
     });
 });
