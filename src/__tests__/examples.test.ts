53be5cb303c14d107ade058c4be90642d0de1a1c
jack <jack.7e@protonmail.com>
2022-12-04 12:31:00 +0000

    updated tests

--- src/__tests__/examples.test.ts
+++ src/__tests__/examples.test.ts
@@ -1,45 +1,45 @@
-import {solutions} from "../solutions"
+import { solutions } from "../solutions";
 
 describe("day01", () => {
-    it("part1", () => {
-      const {part1} = solutions(1, true)
-      expect(part1()).toBe(24000);
-    });
-    it("part2", () => {
-      const {part2} = solutions(1, true)
-      expect(part2()).toBe(45000);
-    });
+  it("part1", () => {
+    const { part1 } = solutions(1, true);
+    expect(part1()).toBe(24000);
+  });
+  it("part2", () => {
+    const { part2 } = solutions(1, true);
+    expect(part2()).toBe(45000);
+  });
 });
 
 describe("day02", () => {
-    it("part1", () => {
-      const {part1} = solutions(2, true)
-      expect(part1()).toBe(15);
-    });
-    it("part2", () => {
-      const {part2} = solutions(2, true)
-      expect(part2()).toBe(12);
-    });
+  it("part1", () => {
+    const { part1 } = solutions(2, true);
+    expect(part1()).toBe(15);
+  });
+  it("part2", () => {
+    const { part2 } = solutions(2, true);
+    expect(part2()).toBe(12);
+  });
 });
 
 describe("day03", () => {
-    it("part1", () => {
-      const {part1} = solutions(3, true)
-      expect(part1()).toBe(157);
-    });
-    it("part2", () => {
-      const {part2} = solutions(3, true)
-      expect(part2()).toBe(70);
-    });
+  it("part1", () => {
+    const { part1 } = solutions(3, true);
+    expect(part1()).toBe(157);
+  });
+  it("part2", () => {
+    const { part2 } = solutions(3, true);
+    expect(part2()).toBe(70);
+  });
 });
 
 describe("day04", () => {
-    it("part1", () => {
-      const {part1} = solutions(4, true)
-      expect(part1()).toBe("changeMe");
-    });
-    it("part2", () => {
-      const {part2} = solutions(4, true)
-      expect(part2()).toBe("changeMe");
-    });
+  it("part1", () => {
+    const { part1 } = solutions(4, true);
+    expect(part1()).toBe(2);
+  });
+  it("part2", () => {
+    const { part2 } = solutions(4, true);
+    expect(part2()).toBe(4);
+  });
 });
---
+++ src/days/day05.ts
@@ -0,0 +1,11 @@
+import { Solution } from "../types/types";
+
+export const day05 = (data: string[]): Solution => {
+  function part1() {
+    return 0;
+  }
+  function part2() {
+    return 0;
+  }
+  return { part1, part2 };
+};
