41789972de0e90272c2b328e6a36ffd73f67f4d6
jack <jack.7e@protonmail.com>
2022-12-04 12:18:34 +0000

    Day 4: Camp Cleanup

--- src/days/day04.ts
+++ src/days/day04.ts
@@ -1,12 +1,42 @@
-import {Solution} from "../types/types"
+import { Solution } from "../types/types";
 
-export const day04 = (data: string[]) : Solution => {
-    function part1(){
-        console.log(data)
-        return 0
-    }
-    function part2(){
-        return 0
-    }
-    return {part1, part2}
+function pairToNumberArray(values): number[] {
+  const [v1, v2] = values.split("-");
+  const start = parseInt(v1.toString());
+  const end = parseInt(v2.toString());
+  return [...Array(end - start + 1).keys()].map((x) => x + start);
 }
+
+export const day04 = (data: string[]): Solution => {
+  function part1() {
+    let pairsThatAreSubsets = 0;
+    data.forEach((pair) => {
+      const [p1, p2] = pair.split(",");
+      const v1 = pairToNumberArray(p1);
+      const v2 = pairToNumberArray(p2);
+      if (v1.every((x) => v2.includes(x))) {
+        pairsThatAreSubsets += 1;
+      } else if (v2.every((x) => v1.includes(x))) {
+        pairsThatAreSubsets += 1;
+      }
+    });
+    return pairsThatAreSubsets;
+  }
+  function part2() {
+    let overlaps = 0;
+    data.forEach((pair) => {
+      const [p1, p2] = pair.split(",");
+      const v1 = pairToNumberArray(p1);
+      const v2 = pairToNumberArray(p2);
+      let foundOverlap = false;
+      v1.forEach((x) => {
+        if (v2.includes(x)) {
+          foundOverlap = true;
+        }
+      });
+      if (foundOverlap) overlaps += 1;
+    });
+    return overlaps;
+  }
+  return { part1, part2 };
+};
