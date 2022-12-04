72e8ccabca4e44ca4398e3b73fcce8eee2c80f4d
jack <jack.7e@protonmail.com>
2022-12-04 12:28:57 +0000

    added peformance log

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,37 @@
 import { solutions } from "./solutions";
+import { PerformanceAndValue } from "./types/types";
 
-function handleArgs(args:string[]) {
-    const dayOptionIndex = args.indexOf('-d')
-    const useExample = args.indexOf('-e') !== -1 ? true : false
-    const day = parseInt(args[dayOptionIndex + 1])
-    
-    if(dayOptionIndex === -1) throw new Error("Missing '-d' option... try '-d 1' for day01");
-    if(Number.isNaN(day)) throw new Error("Missing day value after '-d' try '-d 1' for day01");
-
-    console.log(`AOC for day ${day} ${useExample ? "(using example input)" : ""}`);
-
-    let {part1, part2} = solutions(day, useExample)
-    console.log("part1: ",part1())
-    console.log("part2: ",part2())
+function valueAndPerformance(func: Function): PerformanceAndValue {
+  let start = performance.now();
+  const res = func();
+  let end = performance.now();
+  return { time: end - start, res };
 }
 
-handleArgs(process.argv)
+function handleArgs(args: string[]) {
+  const dayOptionIndex = args.indexOf("-d");
+  const useExample = args.indexOf("-e") !== -1 ? true : false;
+  const day = parseInt(args[dayOptionIndex + 1]);
+
+  if (dayOptionIndex === -1)
+    throw new Error("Missing '-d' option... try '-d 1' for day01");
+  if (Number.isNaN(day))
+    throw new Error("Missing day value after '-d' try '-d 1' for day01");
+
+  console.log(
+    `AOC for day ${day} ${useExample ? "(using example input)" : ""}`
+  );
+
+  let { part1, part2 } = solutions(day, useExample);
+  const p1 = valueAndPerformance(part1);
+  const p2 = valueAndPerformance(part2);
+
+  console.log(
+    `(Time:${p1.time.toFixed(4).padStart(8, " ")}ms) part1: ${p1.res}`
+  );
+  console.log(
+    `(Time:${p2.time.toFixed(4).padStart(8, " ")}ms) part2: ${p2.res}`
+  );
+}
+
+handleArgs(process.argv);
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,11 @@
 export type SolutionCallback = () => any;
 
 export type Solution = {
-    part1: SolutionCallback,
-    part2: SolutionCallback,
-}
+  part1: SolutionCallback;
+  part2: SolutionCallback;
+};
+
+export type PerformanceAndValue = {
+  time: number;
+  res: any;
+};
