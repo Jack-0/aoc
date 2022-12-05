ff509fd227102a17769febff2a68ec1c9df613bb
Jack <harri109@lancaster.ac.uk>
2022-12-05 23:58:05 +0000

    clean code

--- 2022/typescript/src/days/day05.ts
+++ 2022/typescript/src/days/day05.ts
@@ -7,62 +7,31 @@ function cratesTo2DArray(originalCrates:string[]):string[][]{
   // split last array to get last number
   const crateNumbers = crates[crates.length-1].split(" ").filter(x=> x !== '')
   const numOfCrateTowers = parseInt(crateNumbers[crateNumbers.length-1])
-
-
-  // generate 2d array
+  // generate matrix
   const crateArr = Array(numOfCrateTowers).fill('').map(()=>Array().fill(''))
-  console.log(crateArr)
   crates.pop()
   // fill crates
-    for (let heightIdx = crates.length - 1; heightIdx >= 0; heightIdx--) {
-        // console.log(crates[heightIdx])
-        // console.log(heightIdx)
-
-        const row = crates[heightIdx];
-        let pos = 0;
-        for (let rowIdx = 1; rowIdx < row.length; rowIdx+=4)
-        {
-          // console.log(pos, row[rowIdx]);
-          if(row[rowIdx] !== ' ')
-          crateArr[pos].push(row[rowIdx])
-          pos++
-        }
+  for (let heightIdx = crates.length - 1; heightIdx >= 0; heightIdx--) {
+    const row = crates[heightIdx];
+    let pos = 0;
+    for (let rowIdx = 1; rowIdx < row.length; rowIdx+=4)
+    {
+      if(row[rowIdx] !== ' ')
+      crateArr[pos].push(row[rowIdx])
+      pos++
     }
-
-    crateArr.forEach(x=>{
-      console.log(x)
-      console.log(x[x.length - 1])
-    })
-
-    console.log(crateArr[1][crateArr[1].length - 1])
-    // for (let i = 1; i < row.length; i += 1) {
-        // console.log("slice", row.slice(i, i+3))
-      // 
-    // }
-  
-  // parse each row
-
-
-
-
-
+  }
   return crateArr;
 }
 
 function move(crateArr:string[][], n:number, origin:number, target:number, reverse:boolean=false) : string[][]{
-  
   let toMoveArr = crateArr[origin-1].splice(crateArr[origin-1].length - n, crateArr[origin-1].length);
   if(reverse){
     toMoveArr = toMoveArr.reverse()
   }
-  // const toMoveArr = crateArr[origin-1].splice(crateArr[origin-1].length - n, crateArr[origin-1].length).reverse();
-  console.log("a",toMoveArr)
-  console.log(crateArr)
-
   toMoveArr.forEach((x)=>{
     crateArr[target-1].push(x)
   })
-  console.log("b", crateArr)
   return crateArr
 }
 
@@ -81,33 +50,18 @@ function topCrates(crateArr:string[][]) : string {
 }
 
 function parseInstructions(instruction:string) : MoveInstructions{
-
-  console.log("Ins",instruction)
   const splitString = instruction.split(" ")
-  console.log(splitString)
   const amount = parseInt(splitString[1])
   const origin = parseInt(splitString[3])
   const target = parseInt(splitString[5])
-
   return {amount, origin, target}
 }
 
 
 export const day05 = (data: string[]): Solution => {
-  console.log(data)
-
-  // looks like a towers of hanoi type problem
-
-  // parse data
   const splitIndex = data.indexOf('');
   const crates = data.slice(0,splitIndex)
   const moves = data.slice(splitIndex)
-  console.log("crates",crates)
-  
-  
-  // console.log("moves",moves)
-
-
   function part1() {
     let crateArr = cratesTo2DArray(crates);
     moves.forEach(x => {
