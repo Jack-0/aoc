3c850d5ab046f04a01375159fd5978a95cb6b57a
Jack <harri109@lancaster.ac.uk>
2022-12-06 19:57:54 +0000

    cleaned up

--- 2022/typescript/src/days/day06.ts
+++ 2022/typescript/src/days/day06.ts
@@ -1,15 +1,8 @@
 import { Solution } from "../types/types";
 
-// find start of sequence 
-
-// it's after the 4th uniquie character
-
 function findIndexOfNthUniqueChar(n:number, str:string) : number{
   const arr = Array.from(str)
-
   let found:string[] = []
-
-
   for (let idx = 0; idx < arr.length; idx++) {
     const c = arr[idx]
     if (!found.includes(c)){
@@ -17,18 +10,16 @@ function findIndexOfNthUniqueChar(n:number, str:string) : number{
     } else {
       found = []
       found.push(c)
-      console.log(idx,"reset=",found)
     }
     if(found.length === n) {
-      console.log("found", idx + 1)
-      return idx + 1
+      // idx + 1 as elves count from 1 not 0
+      return idx+1
     }
   }
   return 0
 }
 
 export const day06 = (data: string[]): Solution => {
-  console.log(data)
   function part1() {
     return findIndexOfNthUniqueChar(4, data[0])
   }
